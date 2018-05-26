13e5cc3fe1162c473fd5cc23ee85588755336a9b
Charles Harries <charlesgharries@gmail.com>
2018-05-26 21:05:06 +0100

    Redirect http to https

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :redirect_www
+
+  def redirect_www
+  	if request.host == 'www.mountainsofhokkaido.com'
+  		redirect_to 'https://www.mountainsofhokkaido.com' + request.fullpath, :status => 301
+  	end
+  end
 end
